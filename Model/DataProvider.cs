2a38dba2be8e6742f67b3deebb9d89cc78e5fed3
271baotg <21521862@gm.uit.edu.vn>
2022-10-28 08:41:32 +0700

    Login

---
+++ Model/DataProvider.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QUANLICAPHE.Model
+{
+    internal class DataProvider
+    {
+        private static DataProvider _ins;
+        public static DataProvider Ins
+        {
+            get
+            {
+
+                if (_ins == null)
+                    _ins = new DataProvider();
+                return _ins;
+            }
+            set
+            {
+                _ins = value;
+            }
+        }
+        public QUANLICAPHEEntities DB { get; set; }
+        public DataProvider()
+        {
+            DB = new QUANLICAPHEEntities();
+        }
+    }
+}
+
